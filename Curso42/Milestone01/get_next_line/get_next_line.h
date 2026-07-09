650c7ea13cff02a6368a53457f99c7c3fb298920
Blanca Romero Rodriguez <blromero@c3r8s5.42malaga.com>
2026-07-09 09:47:23 +0200

    buffer size variable global

--- Curso42/Milestone01/get_next_line/get_next_line.h
+++ Curso42/Milestone01/get_next_line/get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: blromero <blromero@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/25 16:05:33 by blromero          #+#    #+#             */
-/*   Updated: 2026/06/27 19:00:34 by blromero         ###   ########.fr       */
+/*   Updated: 2026/07/09 09:37:47 by blromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
