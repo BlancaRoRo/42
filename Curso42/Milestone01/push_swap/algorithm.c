6c5f4a6d450b55886706b539a586f35dd10e2446
Blanca Romero Rodriguez <blromero@c3r8s5.42malaga.com>
2026-07-09 13:40:28 +0200

    hacer una prueba con simple y medium

--- Curso42/Milestone01/push_swap/algorithm.c
+++ Curso42/Milestone01/push_swap/algorithm.c
@@ -6,7 +6,7 @@
 /*   By: blromero <blromero@student.42.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/04 16:57:00 by blromero          #+#    #+#             */
-/*   Updated: 2026/07/05 15:13:12 by blromero         ###   ########.fr       */
+/*   Updated: 2026/07/09 13:31:55 by blromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,38 @@ void	simple(t_stack **a)
 		ra(a);
 	}
 }
+void    sort_range(int range_min, int range_max, stack *b, stack *a )
+{
+	while (a->next)
+	{
+		if (range_min <= a->value && range_max >= a->value)
+			pb(a,b);
+		else
+			ra(a);
+	}
+	simple(b);
+        while (b)
+	{
+		pa(a,b);
+		ra(a);
+	}
+}
 
 void	medium(t_stack **a, t_stack **b)
 {
 	int	min;
 	int	max;
+	int	length;
+	t_stack	*franja1;
+	t_stack	*franja2;
+	t_stack	*franja3;
+
+	min = INT_MAX;
+	max = INT_MIN;
+	get_min_max(*a, &min, &max);
+	length = (max + min - 1) / 3;
+	sort_range(min, min + length - 1, b, a);
+	sort_range(min + length, (length +  min) * 2 - 1, b, a);
+	sort_range((min + length * 2), max, b, a);
 }
+
--- Curso42/Milestone01/push_swap/push_swap.h
+++ Curso42/Milestone01/push_swap/push_swap.h
@@ -5,6 +5,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include "./libft/libft.h"
+# include <limits.h>
 
 typedef struct s_stack
 {
@@ -17,5 +18,21 @@ int		has_duplicate(t_stack *a);
 float	compute_disorder(t_stack *a);
 int		is_algorithm(char *argv);
 t_stack	*fill_stack(char **argv, int start);
-
+void		simple(t_stack **a);
+void	sort_range(int range_min, int range_max, t_stack *b, t_stack  *a);
+void	medium(t_stack **a, t_stack **b);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+int	ft_strcmp(char *s1, char *s2);
+int	is_algorithm(char *argv);
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, stack **b);
+int	lst_length(t_stack *a);
+void	sa(t_stack **a);
+void	sb(t_stack **b);
+void	ss(t_stack **a, t_stack **b);
 #endif
